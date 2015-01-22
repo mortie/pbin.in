bcaf695e8fdc7d29f1268ba6056cf9bd9a82fe26
mortie <martin@mort.coffee>
2015-01-22 23:04:04 +0100

    now warns you when accessing a paste which doesn't exist, and doesn't throw PHP notices and warnings anymore

--- view.php
+++ view.php
@@ -1,10 +1,16 @@
 <?php
-$name = $_GET["p"];
+if (empty($_GET["p"]))
+	$name = "";
+else
+	$name = $_GET["p"];
 
 if (preg_match('/[^a-zA-Z0-9\-_]/', $name))
 	die("'$name' contains illegal characters.");
 
-$content = file_get_contents("../pastes/$name");
+if ($name !== "" && file_exists("../pastes/$name"))
+	$content = file_get_contents("../pastes/$name");
+else
+	$content = "404: Paste '$name' doesn't exist.";
 ?>
 <!DOCTYPE html>
 <html>
